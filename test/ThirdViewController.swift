8dfdef1c6ed09896f3b0bdcbb789aec97d174ed2
Kojiro Hamada <kojiro2ph@gmail.com>
2021-12-06 15:17:10 +0900

    add BMI calc

--- test/ThirdViewController.swift
+++ test/ThirdViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 class ThirdViewController: UIViewController {
 
     @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var heightTxtFld: UITextField!
+    @IBOutlet weak var weightTxtFld: UITextField!
+    @IBOutlet weak var resultLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        label3.text = "３つ目の画面"
+        label3.text = "身長と体重を入力して下さい"
+        heightTxtFld.placeholder = "身長を入力して下さい"
+        weightTxtFld.placeholder = "体重を入力して下さい"
+        resultLbl.text = ""
     }
     
 
+    @IBAction func calcButtonAction(_ sender: Any) {
+        let h = Double(heightTxtFld.text!)
+        let w = Double(weightTxtFld.text!)
+        resultLbl.text = calcBMI(height: h!, weight: w!)
+    }
+    
+    func calcBMI(height: Double, weight: Double) -> String {
+        let h = height / 100
+        let w = weight
+        var result = w / (h * h)
+        result = floor(result * 10) / 10
+        return result.description
+        
+    }
+    
+    
     /*
     // MARK: - Navigation
 
